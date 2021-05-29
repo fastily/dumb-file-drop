c2b1ca6d420899452e2855f65304022e296fbf8e
fastily <fastily@users.noreply.github.com>
2021-05-29 03:57:38 -0700

    fix name in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dumb_file_drop",
+    name="dumb-file-drop",
     version="0.0.1",
     author="Fastily",
     author_email="fastily@users.noreply.github.com",
